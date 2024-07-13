d448b42d68cd5b2d45785c0ccac609b397f868da
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-13 09:32:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os, sys
 
-@st.experimental_singleton
+
 os.system('sbase install geckodriver')
 os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
