f36ab3d30747104ec78309bb0bbc780e12a05738
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-13 09:38:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os, sys
-
+import time
 
 os.system('sbase install geckodriver')
 os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
@@ -12,8 +12,10 @@ opts = FirefoxOptions()
 opts.add_argument("--headless")
 browser = webdriver.Firefox(options=opts)
 
-browser.get('https://www.cricbuzz.com/')
-ph=browser.find_element("xpath", '//*[@id="cb-main-menu"]/a[2]')
-st.text(ph.text)
+
+browser.get('https://x.com/i/flow/login/')
+time.sleep(7)
+username = browser.find_element("name", "text")
+username.click()
 st.text("i am okkkk")
 st.write(browser.page_source)
